f5c7422c4c6b0adcd2507e2c82148acf2f7ac1c9
615046 <walter.615046@swl.k12.oh.us>
2025-09-29 08:54:36 -0400

    m

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        //Variables
+        double gallons;
+        double fuelEfficiency;
+        double pricePerGallon;
+
+        do {
+            System.out.println("Enter Current Number of Gallons of Gasoline in Vehicle: ");
+            if (scan.hasNextDouble()) {
+                gallons = scan.nextDouble();
+                System.out.println("Enter Fuel Efficiency in MPG: ");
+                if (scan.hasNextDouble()) {
+                    fuelEfficiency = scan.nextDouble();
+                    System.out.println("Enter Current Price for Gallon of Gas: ");
+                    if (scan.hasNextDouble()) {
+                        pricePerGallon = scan.nextDouble();
+                    }
+                }
+            }
+        } while ()
+
+    }
 }
