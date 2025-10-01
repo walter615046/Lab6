4630751c8044a55f395100a6eb8b4407de2a2160
615046 <walter.615046@swl.k12.oh.us>
2025-10-01 08:50:10 -0400

    m

--- src/Task2.java
+++ src/Task2.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Task2 {
@@ -5,24 +6,55 @@ public class Task2 {
 
         Scanner scan = new Scanner(System.in);
         //Variables
-        double gallons;
-        double fuelEfficiency;
-        double pricePerGallon;
+        double gallons = 0;
+        double fuelEfficiency = 0;
+        double pricePerGallon = 0;
+        boolean validGallons = false;
+        boolean validFuel= false;
+        boolean validPrice =false;
 
         do {
             System.out.println("Enter Current Number of Gallons of Gasoline in Vehicle: ");
             if (scan.hasNextDouble()) {
                 gallons = scan.nextDouble();
-                System.out.println("Enter Fuel Efficiency in MPG: ");
-                if (scan.hasNextDouble()) {
-                    fuelEfficiency = scan.nextDouble();
-                    System.out.println("Enter Current Price for Gallon of Gas: ");
-                    if (scan.hasNextDouble()) {
-                        pricePerGallon = scan.nextDouble();
-                    }
-                }
+                validGallons = true;
+            } else
+                System.out.println("You have entered an invalid value for gallons of gas Please try again.");
+            scan.nextLine();
+            if (gallons < 0) {
+                System.out.println("You have entered an invalid value for gallons of gas Please try again.");
+                validGallons = false;
             }
-        } while ()
+        }while (!validGallons);
+        do {
+            System.out.println("Enter Fuel Efficiency in MPG: ");
+            if (scan.hasNextDouble()) {
+                fuelEfficiency = scan.nextDouble();
+                validFuel = true;
+            } else
+                System.out.println("You have entered an invalid value for Fuel Efficiency. Please try again.");
+            scan.nextLine();
+            if (fuelEfficiency <= 0) {
+                System.out.println("You have entered an invalid value for Fuel Efficiency. Please try again.");
+                validFuel = false;
+            }
+        }while (!validFuel);
+        do {
+            System.out.println("Enter Current Price for Gallon of Gas: ");
+            if (scan.hasNextDouble()) {
+                pricePerGallon = scan.nextDouble();
+                validPrice = true;
+            } else
+                System.out.println("You have entered an invalid value for Price per Gallon. Please try again.");
+            scan.nextLine();
+            if (pricePerGallon <= 0) {
+                System.out.println("You have entered an invalid value for Price per Gallon. Please try again.");
+                validPrice = false;
+            }
+        }while (!validPrice);
 
+        //Give result
+        System.out.printf("%-18s%10s", "Cost per 100 miles in $:", pricePerGallon/fuelEfficiency);
+        System.out.printf("\n%-7s%10s", "Current Price for Gallon of Gas:", gallons*fuelEfficiency);
     }
 }
